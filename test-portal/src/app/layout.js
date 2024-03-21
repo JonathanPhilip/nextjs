72f084eb55b71677f86fb48c4fbad4673718a200
Jonathan <jontyphilipt3@gmail.com>
2024-03-21 17:29:42 +0530

    Add Navbar
    . The navbar includes a name on the left and "create test" button aligned to the right. No additional functionality or styling added at this stage.

--- test-portal/src/app/layout.js
+++ test-portal/src/app/layout.js
@@ -1,5 +1,7 @@
+
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from './_component/_navbar/Navbar';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +13,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+<div>
+        
+        <Navbar className="w-full"/>
+        
+        {children}
+        </div>
+        
+
+      </body>
     </html>
   );
 }
